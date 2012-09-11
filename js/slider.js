9266d5ea3ad47a022219b199bcde8d72853e2783
Jack Watling <jackwatling@gmail.com>
2012-09-11 23:46:24 +0100

    Added Slice class
    A new Slice is created for each slice within the slider. The Slice is
    resposible for building its self, keeping reference of its own index and
    width.

--- js/slider.js
+++ js/slider.js
@@ -1,21 +1,54 @@
 ;(function( $, window, document, undefined ){
 
-	//Slider
-	function Slider( element ){
-		this.element = $( element );
+	//Slice
+	function Slice( index, width ){
+		this.index = index;
+		this.width = width;
 		this.build();
 	}
 
+	Slice.prototype.build = function(){
+		var self = this;
+		var parts = ["front", "top", "back", "bottom", "left", "right"];
+		var slice = $( '<section class="slice"></section>' ).css( { width: this.width } );
+
+		$.each( parts, function( i ){
+			slice.append(
+				$( '<section></section>', { class: this } )
+					.css( this == 'right' ? { left: -400 + self.width } : {} )
+			);
+		});
+
+		this.element = slice;
+	}
+
+	//Slider
+	function Slider( element ){
+		this.element = $( element );
+		this.slice_total = 2;
+		this.slice_width = this.element.width() / this.slice_total;
+		this.slices = new Array();
+		this.build();
+		this.display();
+	}
+
 	Slider.prototype.build = function(){
-		var slides = $( '<section class="slides"></section>' ).appendTo( this.element );
-		var slice = $( '<section class="slice"></section>' )
-						.append('<section class="front"></section>')
-						.append('<section class="top"></section>')
-						.append('<section class="back"></section>')
-						.append('<section class="bottom"></section>')
-						.append('<section class="left"></section>')
-						.append('<section class="right"></section>')
-						.appendTo( slides );
+		//Construct slices
+		for (var i = 0; i < this.slice_total; i++){
+			this.slices.push( new Slice( i, this.slice_width ) );
+		}
+	}
+
+	Slider.prototype.display = function(){
+		var slides = $('<section></section', { 
+			class: 'slides'
+		});
+
+		$.each( this.slices, function() {
+			slides.append( this.element );
+		});
+
+		this.element.append( slides );
 	}
 
 	$.fn.slider = function(){
