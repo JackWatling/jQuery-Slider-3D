d124954e6c5b70bfdb3a109354828f364cd48b82
Jack Watling <jackwatling@gmail.com>
2012-09-11 20:24:56 +0100

    Added Slider class
    This contains a build function which builds, currently, one slice.

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,26 @@
 ;(function( $, window, document, undefined ){
 
+	//Slider
+	function Slider( element ){
+		this.element = $( element );
+		this.build();
+	}
+
+	Slider.prototype.build = function(){
+		var slides = $( '<section class="slides"></section>' ).appendTo( this.element );
+		var slice = $( '<section class="slice"></section>' )
+						.append('<section class="front"></section>')
+						.append('<section class="top"></section>')
+						.append('<section class="back"></section>')
+						.append('<section class="bottom"></section>')
+						.append('<section class="left"></section>')
+						.append('<section class="right"></section>')
+						.appendTo( slides );
+	}
+
 	$.fn.slider = function(){
-		
+		var slider = new Slider( this );
+		return slider;
 	}
 
 })( jQuery, window, document );
